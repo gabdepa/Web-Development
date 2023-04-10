c8614f9288287d68a212cf7a3b5171e4dd3f8bdd
gabdepa <gabriel-r-p-p@hotmail.com>
2023-04-10 12:00:15 -0300

    Updating to use MongoDB

--- todo_list/app.js
+++ todo_list/app.js
@@ -1,51 +1,155 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
 // New Instance of express
 const app = express();
-
-const items = [];
-const workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extende: true }));
 app.use(express.static("public"));
 
+// Connecting to DB
+mongoose.connect("mongodb://localhost:27017/todoListDB", { useNewUrlParser: true });
+
+// Mongoose Schema
+const itemsSchema = {
+    name: String
+};
+
+// Mongoose model
+const Item = mongoose.model("Item", itemsSchema);
+
+// Creating items
+const item1 = new Item({
+    name: "Welcome to your todoList!"
+});
+
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+    name: "<-- Hit this to delete an item."
+});
+
+const listSchema = {
+    name: String,
+    items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
+// Array of the default items
+const defaultItems = [item1, item2, item3];
+
 // Root Route
 app.get("/", function (req, res) {
-    const day = date.getDay();
 
-    // Render "list.ejs" file
-    res.render("home", { listTitle: day, newListItems: items });
+    Item.find({}, function (err, foundItems) {
+        if (foundItems.length === 0) {
+            // Added elements if there is no items yet
+            Item.insertMany(defaultItems);
+            // Redirect back to the Root Route
+            res.redirect("/");
+        }
+        else {
+            // Render "list.ejs" file
+            res.render("home", { listTitle: "Today", newListItems: foundItems.name });
+        }
+    });
+
+
 });
 
+// Differente params Route
+app.get("/:customListName", function (req, res) {
+    // Get the parameter passed
+    const customListName = _.capitalize(req.params.customListName);
+
+    List.findOne({ name: customListName }, function (err, foundList) {
+        if (!err) {
+            if (!foundList) {
+                // Create a new List
+                const list = new List({
+                    name: customListName,
+                    items: defaultItems
+                });
+
+                // Save the created list
+                list.save();
+                // Redirect user to the List
+                res.redirect("/" + customListName);
+            }
+            else {
+                // Show existing list
+                res.render("list", { listTitle: foundList.name, newListItems: foundList.items });
+            }
+        }
+    })
+
+
+
+});
+
+// Main Post Route
 app.post("/", function (req, res) {
 
-    const item = req.body.newItem;
+    // Get the name of the new item inserted
+    const itemName = req.body.newItem;
+    // Get the list name of the item inserted
+    const listName = req.body.list;
+    // Creating new document for MongoDB
+    const item = new Item({
+        name: itemName
+    });
 
-    if (req.body.list === "Work List") {
-        workItems.push(item);
-        res.redirect("/work");
-    }
-    else {
-        items.push(item);
+    if (listName === "Today") {
+        // Save to the DB
+        item.save();
+        // Redirect to Root Route to render
         res.redirect("/");
     }
+    else {
+        List.findOne({ name: listName }, function (err, foundList) {
+            foundList.items.push(item);
+            foundList.save();
+            res.redirect("/" + listName);
+        });
+    }
 });
 
-// Work Route
-app.get("/work", function (req, res) {
-    res.render("list", { listTitle: "Work List", newListItems: workItems });
+// Main Delete items route
+app.post("/delete", function (req, res) {
+    const checkedItemID = req.body.checkbox;
+    const listName = req.body.listName;
+
+    if (listName === "Today") {
+        Item.findByIdAndRemove(checkedItemID, function (err) {
+            if (!err) {
+                console.log("Successfully deleted");
+                res.redirect("/" + listName);
+            } 
+        });
+    }
+    else {
+        List.findOneAndUpdate(
+            { name: listName },
+            { $pull: { items: { _id: checkedItemID } } },
+            function (err, foundList){
+                if(!err){
+                    res.redirect("/" + listName);
+                }
+            });
+    }
 });
 
-app.post("/work", function (req, res) {
-    const workItem = req.body.newItem;
-
-    workItems.push(workItem);
-
-    res.redirect("/work");
+// About Route
+app.get("/about", function (req, res) {
+    res.render("about", { listTitle: "About List", newListItems: workItems });
 });
 
 app.listen(3000, function () {
