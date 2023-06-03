cb70799e2d873cf496dd486a6267e9434bbf74ea
gabdepa <gabriel-r-p-p@hotmail.com>
2023-06-03 10:55:33 -0300

    Changed methods to chained route

--- restful_API/app.js
+++ restful_API/app.js
@@ -38,56 +38,56 @@ const articleSchema = {
 // The first argument is the singular name of the collection your model is for. Mongoose automatically looks for the plural, lowercased version of your model name.
 const Article = mongoose.model("Article", articleSchema);
 
-// Handle GET requests to the "/articles" route
-app.get("/articles", function (req, res) {
-  // Use the Article model to find all articles in the database
-  Article.find()
-    // Once the find operation is done, this function will be called with the found articles as its argument
-    .then(function (foundArticles) {
-      // Send the found articles back to the client as the response
-      res.send(foundArticles);
-    })
-    // If an error occurred during the find operation, this function will be called with the error as its argument
-    .catch(function (err) {
-      // Send the error back to the client as the response
-      res.send(err);
-      // Handle the error appropriately here
+app.route("/articles")
+  // Handle GET requests to the "/articles" route
+  .get(function (req, res) {
+    // Use the Article model to find all articles in the database
+    Article.find()
+      // Once the find operation is done, this function will be called with the found articles as its argument
+      .then(function (foundArticles) {
+        // Send the found articles back to the client as the response
+        res.send(foundArticles);
+      })
+      // If an error occurred during the find operation, this function will be called with the error as its argument
+      .catch(function (err) {
+        // Send the error back to the client as the response
+        res.send(err);
+        // Handle the error appropriately here
+      });
+  })
+  // Handle POST requests to the "/articles" route
+  .post(function (req, res) {
+    // Create a new article instance using the title and content from the request body
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
     });
-});
 
-// Handle POST requests to the "/articles" route
-app.post("/articles", function (req, res) {
-  // Create a new article instance using the title and content from the request body
-  const newArticle = new Article({
-    title: req.body.title,
-    content: req.body.content,
+    // Try to save the new article in the database
+    newArticle
+      .save()
+      // If the article was saved successfully, send a success message to the client
+      .then(function () {
+        res.send("Successfully added new article!");
+      })
+      // If an error occurred during the save operation, send the error to the client
+      .catch(function (err) {
+        res.send(err);
+      });
+  })
+  // Handle DELETE requests to the "/articles" route
+  .delete(function (req, res) {
+    // Use the Article model to delete all documents in the collection
+    Article.deleteMany()
+      // If the delete operation was successful, send a success message to the client
+      .then(function () {
+        res.send("Successfully deleted all records!");
+      })
+      // If an error occurred during the delete operation, send the error to the client
+      .catch(function (err) {
+        res.send(err);
+      });
   });
-  // Try to save the new article in the database
-  newArticle
-    .save()
-    // If the article was saved successfully, send a success message to the client
-    .then(function () {
-      res.send("Successfully added new article!");
-    })
-    // If an error occurred during the save operation, send the error to the client
-    .catch(function (err) {
-      res.send(err);
-    });
-});
-
-// Handle DELETE requests to the "/articles" route
-app.delete("/articles", function (req, res) {
-  // Use the Article model to delete all documents in the collection
-  Article.deleteMany()
-    // If the delete operation was successful, send a success message to the client
-    .then(function () {
-      res.send("Successfully deleted all records!");
-    })
-    // If an error occurred during the delete operation, send the error to the client
-    .catch(function (err) {
-      res.send(err);
-    });
-});
 
 // Make the Express application listen for HTTP requests on port 3000, logging a message to the console once the server is running
 app.listen(3000, function () {
