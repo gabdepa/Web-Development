7b6bc3973f1e5ce820567139caacf781c251f2cf
gabdepa <gabriel-r-p-p@hotmail.com>
2023-06-06 11:14:33 -0300

    Added level 4 of security

--- secrets/app.js
+++ secrets/app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -22,30 +23,34 @@ const userSchema = new mongoose.Schema({
 const User = new mongoose.model("User", userSchema);
 
 app.post("/register", function (req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password),
-  });
-
-  newUser
-    .save()
-    .then(function () {
-      res.render("secrets");
-    })
-    .catch(function (error) {
-      console.log(error);
+  bcrypt.hash(plainText, saltRounds, function (err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash,
     });
+
+    newUser
+      .save()
+      .then(function () {
+        res.render("secrets");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  });
 });
 
 app.post("/login", function (req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({ email: username })
     .then(function (foundUser) {
-      if (foundUser.password === password) {
-        res.render("secrets");
-      }
+      bcrypt.compare(password, foundUser.password, function (err, result) {
+        if (result === true) {
+          res.render("secrets");
+        }
+      });
     })
     .catch(function (error) {
       console.log(error);
