033d99707e1d689e319851af3d9b486feef20c39
gabdepa <gabriel-r-p-p@hotmail.com>
2023-06-07 18:39:16 -0300

    Added brief comments

--- secrets/app.js
+++ secrets/app.js
@@ -1,56 +1,61 @@
 //jshint esversion:6
-require("dotenv").config();
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const mongoose = require("mongoose");
-const passport = require("passport");
-const passportLocalMongoose = require("passport-local-mongoose");
-const session = require("express-session");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const findOrCreate = require("mongoose-findorcreate");
+require("dotenv").config(); // load environment variables from a .env file into process.env
+const express = require("express"); // include express library for server setup
+const bodyParser = require("body-parser"); // include body-parser to parse incoming request bodies
+const ejs = require("ejs"); // include ejs for embedding JavaScript in HTML templates
+const mongoose = require("mongoose"); // include mongoose for MongoDB object modeling
+const passport = require("passport"); // include passport for user authentication
+const passportLocalMongoose = require("passport-local-mongoose"); // include passport-local-mongoose for Passport-Local Mongoose strategy
+const session = require("express-session"); // include express-session for session middleware
+const GoogleStrategy = require("passport-google-oauth20").Strategy; // include passport-google-oauth20 for Google OAuth 2.0 authentication strategy
+const findOrCreate = require("mongoose-findorcreate"); // include mongoose-findorcreate for Mongoose findOrCreate plugin
 
-const app = express();
+const app = express(); // create an express application
 
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public")); // serve static files in the 'public' directory
+app.set("view engine", "ejs"); // set view engine to 'ejs'
+app.use(bodyParser.urlencoded({ extended: true })); // use bodyParser middleware to parse incoming request bodies
 
 app.use(
+  // use express-session middleware
   session({
     secret: "Our little secret.",
     resave: false,
     saveUninitialized: false,
   })
 );
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize()); // initialize Passport authentication middleware
+app.use(passport.session()); // use Passport session middleware
 
-mongoose.connect("mongodb://localhost:27017/userDB");
+mongoose.connect("mongodb://localhost:27017/userDB"); // connect to MongoDB at the specified URI
 
+// define a Mongoose schema for User model
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
   googleId: String,
 });
 
-userSchema.plugin(passportLocalMongoose);
-userSchema.plugin(findOrCreate);
+userSchema.plugin(passportLocalMongoose); // add passport-local-mongoose plugin to the schema
+userSchema.plugin(findOrCreate); // add findOrCreate plugin to the schema
 
-const User = new mongoose.model("User", userSchema);
+const User = new mongoose.model("User", userSchema); // create a User model
 
-passport.use(User.createStrategy());
+passport.use(User.createStrategy()); // create a local authentication strategy
 
+// serialize user instance to the session
 passport.serializeUser(function (user, done) {
   done(null, user.id);
 });
 
+// deserialize user instance from the session
 passport.deserializeUser(function (id, done) {
   User.findById(id)
     .then((user) => done(null, user))
     .catch((err) => done(err));
 });
 
+// create a Google OAuth 2.0 authentication strategy
 passport.use(
   new GoogleStrategy(
     {
@@ -68,6 +73,7 @@ passport.use(
   )
 );
 
+// handle logout requests
 app.get("/logout", function (req, res) {
   req.logout(function (err) {
     if (err) {
@@ -78,6 +84,7 @@ app.get("/logout", function (req, res) {
   });
 });
 
+// handle registration requests
 app.post("/register", function (req, res) {
   User.register({ username: req.body.username }, req.body.password, function (
     err,
@@ -94,6 +101,7 @@ app.post("/register", function (req, res) {
   });
 });
 
+// handle login requests
 app.post("/login", function (req, res) {
   const user = new User({
     username: req.body.username,
@@ -111,15 +119,18 @@ app.post("/login", function (req, res) {
   });
 });
 
+// render home page
 app.get("/", function (req, res) {
   res.render("home");
 });
 
+// handle Google authentication requests
 app.get(
   "/auth/google",
   passport.authenticate("google", { scope: ["profile"] })
 );
 
+// handle Google authentication callback requests
 app.get(
   "/auth/google/secrets",
   passport.authenticate("google", { failureRedirect: "/login" }),
@@ -129,14 +140,17 @@ app.get(
   }
 );
 
+// render login page
 app.get("/login", function (req, res) {
   res.render("login");
 });
 
+// render registration page
 app.get("/register", function (req, res) {
   res.render("register");
 });
 
+// handle requests for the secrets page
 app.get("/secrets", function (req, res) {
   if (req.isAuthenticated()) {
     res.render("secrets");
@@ -145,6 +159,7 @@ app.get("/secrets", function (req, res) {
   }
 });
 
+// start the server
 app.listen(3000, function () {
   console.log("Server is running on port 3000.");
 });
