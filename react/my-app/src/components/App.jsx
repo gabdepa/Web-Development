82723f1e704205ae38cb32bf30c08cceb5f76169
gabdepa <gabriel-r-p-p@hotmail.com>
2023-06-26 15:03:39 -0300

    Final modifications

--- react/my-app/src/components/App.jsx
+++ react/my-app/src/components/App.jsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(note) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, note];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index != id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      {notes.map((noteItem) => {
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
         return (
           <Note
-            key={noteItem.key}
+            key={index}
+            id={index}
             title={noteItem.title}
             content={noteItem.content}
+            onDelete={deleteNote}
           />
         );
       })}
--- react/my-app/src/components/Note.jsx
+++ react/my-app/src/components/Note.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
 function Note(props) {
+  function handleCLick() {
+    props.onDelete(props.id);
+  }
+
   return (
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
+      <button onClick={handleCLick}>DELETE</button>
     </div>
   );
 }
