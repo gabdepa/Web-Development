0226c43d5e8e8a9e85fe6c4443ead5a0649247bd
gabdepa <gabriel-r-p-p@hotmail.com>
2023-07-27 11:27:58 -0300

    Project done

--- cryptochain/index.js
+++ cryptochain/index.js
@@ -7,6 +7,7 @@ const PubSub = require("./app/pubsub");
 const TransactionPool = require("./wallet/transaction-pool");
 const Wallet = require("./wallet");
 const TransactionMiner = require("./app/transaction-miner");
+const { isBooleanObject } = require("util/types");
 
 const isDevelopment = process.env.ENV === "development";
 
